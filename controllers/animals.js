303dcaf4e55a95fbd87a90d2d043923978b3244c
Ross Melville <rossmelville@hotmail.com>
2017-09-12 14:14:12 +0100

    Updated controllers and html file

---
+++ controllers/animals.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var animalsRouter = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var db;
+
+MongoClient.connect('mongodb://localhost:27017/farm', function(err, database){
+  if(err) return;
+  db = database;
+});
+
+
+//animals index
+animalsRouter.get('/', function(req, res) {
+  //Get animals from DB
+  db.collection('animals').find().toArray( function(err, results){
+    if(err) console.log("Ooops " + err.toString());
+    res.json(results);
+  });
+});
+
+//SHOW
+
+//EDIT
+
+//UPDATE
+
+//CREATE
+
+//DELETE
+
+module.exports = animalsRouter;
